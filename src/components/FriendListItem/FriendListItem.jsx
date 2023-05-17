d1b1380b8e072c478a8e661639dbcfc16d5ab43f
Ruslan <113464399+Ruslan1204@users.noreply.github.com>
2023-05-17 15:11:59 +0300

    Update FriendListItem.jsx

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -14,5 +14,5 @@ export const FriendListItem = ({ avatar, name, isOnline }) => {
 FriendListItem.propTypres = {
   avatar: PropTypres.string,
   name: PropTypres.string,
-  // isOnline: PropTypes.bool,
+  isOnline: PropTypres.bool,
 };
