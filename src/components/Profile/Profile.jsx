6e16585092ef39027ba56ef4ccf1199eb3a40df9
Ruslan <113464399+Ruslan1204@users.noreply.github.com>
2023-05-19 11:57:59 +0300

    Update Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -36,7 +36,7 @@ Profile.propTypes = {
   tag: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
   avatar: PropTypes.string.isRequired,
-  stats: PropTypes.objectOf({
+  stats: PropTypes.shape({
     followers: PropTypes.number.isRequired,
     views:PropTypes.number.isRequired,
     likes:PropTypes.number.isRequired,
